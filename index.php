1595bd7bb7208dd89b1e72f58b8d3249c91344f8
Emir Marcelo Freiberger <contato@e1000r.com>
2022-06-09 19:54:03 -0300

    início

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+// https://www.gov.br/compras/pt-br/acesso-a-informacao/noticias
+// Extrair as informações necessárias (manchete, data/hora e link dos detalhes)
+// Buscar notícias até a página 5
+// Salvar esses dados estruturados em um banco de dados ou num arquivo csv
+// Garantir que as notícias não se dupliquem no registro final
+?>
